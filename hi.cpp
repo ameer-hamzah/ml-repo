cbb724b92454457b42c5c8e19bc8fbc2bc621bad
a-hamzah <Spellbreaker907@gmail.com>
2023-07-17 06:20:38 +0500

    frist

---
+++ hi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, this is Hamza. " << endl;
+    cout << "This is my first C++ Program. " << endl;
+    cout << "Updated date : 27 NOV 2022" << endl;
+    cout << "Star Learning!";
+    return 0;
+}
